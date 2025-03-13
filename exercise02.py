7447ec7fd8a7935fe9d8421b262d674075a4df11
Mark Peters <mapeters101@earthlink.net>
2025-03-13 17:35:33 -0400

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,7 @@
+'''
+Question 2
+Expand on your class above to implement equality (==) comparisons between
+instances of your class.
+
+Two accounts should be considered equal if the account numbers are the same.
+'''
